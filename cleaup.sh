401599fcfb4ff0d5499193dff3d48ae044655b4b
C. David Burgess <cdburgess75@gmail.com>
2017-12-24 13:02:45 -0600

    q

--- cleaup.sh
+++ cleaup.sh
@@ -3,7 +3,17 @@
 echo "==> Disk usage before cleanup";
 df -h;
 
+echo "==> ZFS Disk usage before cleanup";
+zfs list;
+
 # Purge files we don't need any longer
+echo "==> Removing FreeBSD update files";
+rm -rf /var/db/freebsd-update/files;
+mkdir -p /var/db/freebsd-update/files;
+rm -f /var/db/freebsd-update/*-rollback;
+rm -rf /var/db/freebsd-update/install.*;
+echo "==> Removing old kernel";
+rm -rf /boot/kernel.old;
 echo "==> Removing sources";
 rm -rf /usr/src/*;
 echo "==> Removing core dumps";
@@ -11,3 +21,6 @@ rm -f /*.core;
 
 echo "==> Disk usage after cleanup";
 df -h;
+
+echo "==> ZFS Disk usage after cleanup";
+zfs list;
