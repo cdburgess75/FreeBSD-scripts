ac84f0e22f553fb615e4c45ac69bf8e9cfc4edee
cdburgess75 <cdburgess75@gmail.com>
2017-12-26 16:42:39 -0600

    Update bhyve.sh

--- bhyve.sh
+++ bhyve.sh
@@ -9,11 +9,9 @@ ifconfig bridge0 create
 ifconfig bridge0 addm igb0 addm tap0
 ifconfig bridge0 up
 echo 'net.link.tap.up_on_open=1' >> /etc/sysctl.conf
-
 echo 'vmm_load="YES"' >> /boot/loader.conf
 echo 'nmdm_load="YES"' >> /boot/loader.conf
 echo 'if_bridge_load="YES"' >> /boot/loader.conf
 echo 'if_tap_load="YES"' >> /boot/loader.conf
-
 echo 'cloned_interfaces="bridge0 tap0"' >> /etc/rc.conf
 echo 'ifconfig_bridge0="addm igb0 addm tap0"' >> /etc/rc.conf
