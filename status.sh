34f21b4ad5a4aec07782a8abea9e9484eb87596f
cdburgess75 <cdburgess75@gmail.com>
2017-12-26 16:51:22 -0600

    Custom FreeBSD script

--- status.sh
+++ status.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#rev1
 configFile="/root/v1/v1.config"
 logFile="$(cat $configFile | awk '/LogFile/ {print $2}')"
 serial="$(/usr/sbin/system_profiler SPHardwareDataType | /usr/bin/awk '/Serial\ Number\ \(system\)/ {print $NF}')"
