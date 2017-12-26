7db62a80e457d877175737d644480fbd4f289b37
cdburgess75 <cdburgess75@gmail.com>
2017-12-26 11:49:41 -0600

    Create call.sh

---
+++ call.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#rev 1
+
+
+Company="";
+Product="";
+Program="";
+Version="";
+VerRoot="";
+Tmp="";
+DTG=".`date +"%Y-%-m-%d-%H:%M:%S"`";
+User="";
+Passwd="";
+UpdateRepo="";
+BuildRepo="";
+ImgDir="";
+Action="";
+
+
+clear
+echo $STR
+echo $STR
+echo "  +--------------------------------------------------------------------+  "
+echo "            $Company - $Product $Version -  $Program $date             "
+echo "  +--------------------------------------------------------------------+  "
+echo $STR
+echo $STR
+echo "$Product - $Action"
+sleep 3
