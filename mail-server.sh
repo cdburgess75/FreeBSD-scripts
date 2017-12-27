f324ba4d4cdf114a1ec4c68b25eed8664e3f67dc
cdburgess75 <cdburgess75@gmail.com>
2017-12-27 12:11:37 -0600

    Update mail-server.sh

--- mail-server.sh
+++ mail-server.sh
@@ -1,22 +1,41 @@
 pkg update -f
 pkg upgrade -f
-pkg install -f ap24-mod_security apache24 apr arc arj autoconf autoconf-wrapper automake automake-wrapper bash bison ca_root_nss cclient clamav-milter cmake cmake-modules-webos kf5-extra-cmake-modules
-pkg install -f curl cvsps davical db5 dialog4ports dovecot-pigeonhole dovecot2 expat ezjail freetype2 gdbm gettext-runtime gettext-tools git gmake gnupg1 help2man icu indexinfo
-pkg install -f jpeg-turbo json-c jsoncpp kbproto ldns lha libICE libSM libX11 libXau libXaw libXdmcp libXext libXmu libXp libXpm libXt libarchive libedit libevent libffi libiconv
-pkg install -f libidn libltdl liblz4 libmcrypt libpthread-stubs libspf2 libtool libuv libxcb libxml2 libzip lua52 lzo2 m4 mod_php56 munin-common mysql56-client mysql56-server nano
-pkg install -f oniguruma5 opendkim opendmarc p5-Algorithm-C3 p5-Authen-NTLM p5-Authen-SASL p5-B-Hooks-EndOfScope p5-BerkeleyDB p5-Bit-Vector p5-Cache p5-Cache-Cache p5-Carp-Clan
-pkg install -f p5-Class-C3 p5-Class-Data-Inheritable p5-Class-Inspector p5-Class-Method-Modifiers p5-Class-Singleton p5-Crypt-CBC p5-Crypt-DES p5-Crypt-OpenSSL-Bignum p5-Crypt-OpenSSL-RSA
-pkg install -f p5-Crypt-OpenSSL-Random p5-DBD-Pg p5-DBD-mysql p5-DBI p5-Data-OptList p5-Date-Calc p5-DateTime p5-DateTime-HiRes p5-DateTime-Locale p5-DateTime-TimeZone p5-Devel-StackTrace
-pkg install -f p5-Digest-HMAC p5-Digest-SHA1 p5-Dist-CheckConflicts p5-Encode-Detect p5-Encode-Locale p5-Error p5-Eval-Closure p5-Exception-Class p5-File-Listing p5-File-NFSLock p5-File-ShareDir
-pkg install -f p5-Filter p5-GSSAPI p5-HTML-Parser p5-HTML-Tagset p5-HTTP-Cookies p5-HTTP-Daemon p5-HTTP-Date p5-HTTP-Message p5-HTTP-Negotiate p5-Heap p5-IO-HTML p5-IO-Multiplex p5-IO-Socket-INET6
-pkg install -f p5-IO-Socket-IP p5-IO-Socket-SSL p5-IO-String p5-IO-stringy p5-IPC-ShareLite p5-LWP-MediaTypes p5-List-AllUtils p5-List-SomeUtils p5-List-SomeUtils-XS p5-List-UtilsBy p5-Locale-gettext
-pkg install -f p5-MRO-Compat p5-Mail-DKIM p5-Mail-SPF p5-Mail-Tools p5-Module-Build p5-Module-Implementation p5-Module-Runtime p5-Mozilla-CA p5-Net-CIDR p5-Net-DNS p5-Net-DNS-Resolver-Programmable p5-Net-HTTP
-pkg install -f p5-Net-IDN-Encode p5-Net-LibIDN p5-Net-SMTP-SSL p5-Net-SNMP p5-Net-SSLeay p5-Net-Server p5-NetAddr-IP p5-Package-Stash p5-Package-Stash-XS p5-Params-Util p5-Params-Validate
-pkg install -f p5-Params-ValidationCompiler p5-Parse-Syslog p5-Role-Tiny p5-Scalar-List-Utils p5-Socket p5-Socket6 p5-Specio p5-Sub-Exporter p5-Sub-Exporter-Progressive p5-Sub-Identify p5-Sub-Install p5-Switch
-pkg install -f p5-TimeDate p5-Try-Tiny p5-URI p5-Variable-Magic p5-WWW-RobotRules p5-XML-LibXML p5-XML-NamespaceSupport p5-XML-Parser p5-XML-SAX p5-XML-SAX-Base p5-YAML p5-libwww p5-namespace-autoclean
-pkg install -f p5-namespace-clean pcre pear pear-Auth pear-Auth_SASL pear-Net_SMTP pear-Net_Socket pecl-intl perl5 pflogsumm php-libawl php56 php56-calendar php56-ctype php56-curl php56-dom php56-extensions
-pkg install -f php56-filter php56-gd php56-gettext php56-hash php56-iconv php56-imap php56-json php56-mbstring php56-mcrypt php56-mysql php56-mysqli php56-opcache php56-openssl php56-pdo php56-pdo_mysql
-pkg install -f php56-pdo_pgsql php56-pdo_sqlite php56-pgsql php56-phar php56-posix php56-session php56-simplexml php56-sqlite3 php56-tokenizer php56-xml php56-xmlreader php56-xmlwriter php56-zip php56-zlib pkg
-pkg install -f pkgconf png portmaster postfix postgresql93-client postgresql93-server postgrey printproto pwgen py27-Babel py27-Jinja2 py27-MarkupSafe py27-alabaster py27-authres py27-dns py27-docutils py27-fail2ban
-pkg install -f py27-imagesize py27-ipaddr py27-postfix-policyd-spf-python py27-pygments py27-pyspf py27-pystemmer py27-pytz py27-setuptools py27-six py27-snowballstemmer py27-sphinx py27-sphinx_rtd_theme py27-sqlite3
-pkg install -f python2 python27 re2c readline rhash rsync scons screen spamassassin sqlite3 t1lib unzoo vsftpd-ssl xextproto xproto yajl
+pkg install -y ap24-mod_security apache24 apr arc arj autoconf autoconf-wrapper automake automake-wrapper bash bison ca_root_nss cclient clamav-milter cmake cmake-modules-webos kf5-extra-cmake-modules curl cvsps davical db5 dialog4ports dovecot-pigeonhole dovecot expat ezjail freetype2 gdbm gettext-runtime gettext-tools git gmake gnupg1 help2man icu indexinfo jpeg-turbo json-c jsoncpp kbproto ldns lha libICE libSM libX11 libXau libXaw libXdmcp libXext libXmu libXp libXpm libXt libarchive libedit libevent libffi libiconv libidn libltdl liblz4 libmcrypt libpthread-stubs libspf2 libtool libuv libxcb libxml2 libzip lua52 lzo2 m4 mod_php56 munin-common mysql56-client mysql56-server nano oniguruma5 opendkim opendmarc p5-Algorithm-C3 p5-Authen-NTLM p5-Authen-SASL p5-B-Hooks-EndOfScope p5-BerkeleyDB p5-Bit-Vector p5-Cache p5-Cache-Cache p5-Carp-Clan p5-Class-c3 p5-Class-Data-Inheritable p5-Class-Inspector p5-Class-Method-Modifiers p5-Class-Singleton p5-Crypt-CBC p5-Crypt-DES p5-Crypt-OpenSSL-Bignum p5-Crypt-OpenSSL-RSA p5-Crypt-OpenSSL-Random p5-DBD-Pg p5-DBD-mysql p5-DBI p5-Data-OptList p5-Date-Calc p5-DateTime p5-DateTime-HiRes p5-DateTime-Locale p5-DateTime-TimeZone p5-Devel-StackTrace
+pkg install -y p5-Digest-HMAC p5-Digest-SHA1 p5-Dist-CheckConflicts p5-Encode-Detect p5-Encode-Locale p5-Error p5-Eval-Closure p5-Exception-Class p5-File-Listing p5-File-NFSLock p5-File-ShareDir
+pkg install -y p5-Filter p5-GSSAPI p5-HTML-Parser p5-HTML-Tagset p5-HTTP-Cookies p5-HTTP-Daemon p5-HTTP-Date p5-HTTP-Message p5-HTTP-Negotiate p5-Heap p5-IO-HTML p5-IO-Multiplex p5-IO-Socket-INET6
+pkg install -y p5-IO-Socket-IP p5-IO-Socket-SSL p5-IO-String p5-IO-stringy p5-IPC-ShareLite p5-LWP-MediaTypes p5-List-AllUtils p5-List-SomeUtils p5-List-SomeUtils-XS p5-List-UtilsBy p5-Locale-gettext
+pkg install -y p5-MRO-Compat p5-Mail-DKIM p5-Mail-SPF p5-Mail-Tools p5-Module-Build p5-Module-Implementation p5-Module-Runtime p5-Mozilla-CA p5-Net-CIDR p5-Net-DNS p5-Net-DNS-Resolver-Programmable p5-Net-HTTP
+pkg install -y p5-Net-IDN-Encode p5-Net-LibIDN p5-Net-SMTP-SSL p5-Net-SNMP p5-Net-SSLeay p5-Net-Server p5-NetAddr-IP p5-Package-Stash p5-Package-Stash-XS p5-Params-Util p5-Params-Validate
+pkg install -y p5-Params-ValidationCompiler p5-Parse-Syslog p5-Role-Tiny p5-Scalar-List-Utils p5-Socket p5-Socket6 p5-Specio p5-Sub-Exporter p5-Sub-Exporter-Progressive p5-Sub-Identify p5-Sub-Install p5-Switch
+pkg install -y p5-TimeDate p5-Try-Tiny p5-URI p5-Variable-Magic p5-WWW-RobotRules p5-XML-LibXML p5-XML-NamespaceSupport p5-XML-Parser p5-XML-SAX p5-XML-SAX-Base p5-YAML p5-libwww p5-namespace-autoclean
+pkg install -y p5-namespace-clean pcre pear pear-Auth pear-Auth_SASL pear-Net_SMTP pear-Net_Socket pecl-intl perl5 pflogsumm php-libawl php56 php56-calendar php56-ctype php56-curl php56-dom php56-extensions
+pkg install -y php56-filter php56-gd php56-gettext php56-hash php56-iconv php56-imap php56-json php56-mbstring php56-mcrypt php56-mysql php56-mysqli php56-opcache php56-openssl php56-pdo php56-pdo_mysql
+pkg install -y php56-pdo_pgsql php56-pdo_sqlite php56-pgsql php56-phar php56-posix php56-session php56-simplexml php56-sqlite3 php56-tokenizer php56-xml php56-xmlreader php56-xmlwriter php56-zip php56-zlib pkg
+pkg install -y pkgconf png portmaster postfix postgresql93-client postgresql93-server postgrey printproto pwgen py27-Babel py27-Jinja2 py27-MarkupSafe py27-alabaster py27-authres py27-dns py27-docutils py27-fail2ban
+pkg install -y py27-imagesize py27-ipaddr py27-postfix-policyd-spf-python py27-pygments py27-pyspf py27-pystemmer py27-pytz py27-setuptools py27-six py27-snowballstemmer py27-sphinx py27-sphinx_rtd_theme py27-sqlite3
+pkg install -y python2 python27 re2c readline rhash rsync scons screen spamassassin sqlite3 t1lib unzoo vsftpd-ssl xextproto xproto yajl
+
+
+
+echo 'zfs_enable="YES" # For ZFS filesystems' >> /etc/rc.conf
+echo 'ntpd_enable="YES"' >> /etc/rc.conf
+echo 'sshd_enable="YES"' >> /etc/rc.conf
+echo 'sendmail_enable="NO"' >> /etc/rc.conf
+echo 'sendmail_submit_enable="NO"' >> /etc/rc.conf
+echo 'sendmail_outbound_enable="NO"' >> /etc/rc.conf
+echo 'sendmail_msp_queue_enable="NO"' >> /etc/rc.conf
+echo 'mysql_enable="YES"' >> /etc/rc.conf
+echo 'postgresql_enable="YES"' >> /etc/rc.conf
+echo 'apache24_enable="YES"' >> /etc/rc.conf
+echo 'postfix_enable="YES"' >> /etc/rc.conf
+echo 'dovecot_enable="YES"' >> /etc/rc.conf
+echo 'spamd_enable="YES"' >> /etc/rc.conf
+echo 'spamd_flags="-d -m5 -x -q -Q -u nobody"' >> /etc/rc.conf
+echo 'milteropendkim_enable="YES"' >> /etc/rc.conf
+echo 'milteropendkim_uid="opendkim"' >> /etc/rc.conf
+echo 'opendmarc_enable="YES"' >> /etc/rc.conf
+echo 'opendmarc_runas="opendmarc"' >> /etc/rc.conf
+echo 'clamav_clamd_enable="YES"' >> /etc/rc.conf
+echo 'clamav_freshclam_enable="YES"' >> /etc/rc.conf
+echo 'clamav_milter_enable="YES"' >> /etc/rc.conf
+echo 'loginscript_enable="YES"' >> /etc/rc.conf
