b8b4627d596bd259cca09b172540e20204533e8d
cdburgess75 <cdburgess75@gmail.com>
2018-05-27 17:08:40 -0500

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,18 +1,13 @@
 #!/bin/tcsh
-echo "FreeNAS Gogs installation script."
-echo "This has been tested on:"
-echo "    r295946+07c41cd(9.10-STABLE)"
+echo "Fortress installation script."
 echo
 echo "Press any key to begin"
 set jnk = $<
 
-if ( -f /usr/local/etc/rc.d/gogs ) then
-    echo "Updating Gogs..."
+if ( -f /root/fortress ) then
+    echo "Beginning Fortress Installation..."
     echo
-    # Stop Gogs service
-    service gogs stop 
-    ./gogs-compile.sh
-    echo "Update Done!"
+    ./root/fortress/update.sh
 else
     # 3) Enable SSH
     echo "Enabling SSH"
@@ -29,35 +24,6 @@ else
     echo "Upgrading packages"
     /usr/sbin/pkg upgrade -y
     echo "Installing memcached, redis & go"
-    /usr/sbin/pkg install -y memcached redis go git bash
-    echo "Enabling & starting memcached & redis"
-    sysrc memcached_enable=YES
-    sysrc redis_enable=YES
-
-    /usr/sbin/service memcached start
-    /usr/sbin/service redis start
-    # 5) Create user first; installing git will install a git user to 913
-    echo "Creating git user"
-    setenv GITHOME /usr/home/git/
-    mkdir -p $GITHOME
-    if (! -d /home ) then
-	    /bin/ln -s /usr/home /home
-    endif
-    pw add user -n git -u 913 -d $GITHOME -s /bin/tcsh -c "Gogs -  Go Git Service"
-    chown -R git:git $GITHOME
-    su - git -c "/usr/bin/ssh-keygen -b 2048 -N '' -f ~/.ssh/id_rsa -t rsa -q &"
-    # 6) Get & compile gogs
-    ./gogs-compile.sh
-    su - git -c "ln -s /usr/home/git/.ssh/ /usr/home/git/gogs/"
-    # 7) Start up scripts
-    sysrc gogs_enable=YES
-endif
-# cleaning this mess
-su - git -c "rm -rf /usr/home/git/go/"
-service gogs start
-echo 
-echo
-echo
-
-echo "Gogs should be running on port 3000 on the following addresses:"
-ifconfig | grep inet
+    /usr/sbin/pkg install -y wget
+   
+  
